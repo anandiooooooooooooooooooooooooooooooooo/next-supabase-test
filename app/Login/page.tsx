b35ee76fdfe3971ed76549465e1a090d907c87e7
Kaidodigit <zidaneizd@gmail.com>
2025-07-13 17:52:00 +0700

    [feat] LoginPage 

---
+++ app/Login/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button";
+
+export default function HomePage() {
+  return (
+    
+    <div>
+      <Card>
+  <CardHeader>
+    <CardTitle>Login</CardTitle>
+    <CardDescription>Email</CardDescription>
+    <CardAction>Lu gapunya akun? Daftar aja bro!</CardAction>
+  </CardHeader>
+  <CardContent>
+    <form>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="blablabla@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <a
+                  href="#"
+                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+          </div>
+        </form>
+  </CardContent>
+  <CardFooter>
+    <Button type="submit" className="w-full">
+          Login
+        </Button>
+        <Button variant="outline" className="w-full">
+          Login with Google
+        </Button>
+  </CardFooter>
+</Card>
+    </div>
+
+  );
+}
