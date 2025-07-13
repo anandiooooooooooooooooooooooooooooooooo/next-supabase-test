97f64c56967467e3715c9bb1fcad8fedd20686d0
anandiooooooooooooooooooooooooooooooooo <anandhio123@gmail.com>
2025-07-13 16:34:33 +0700

    starter

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import Navbar from "@/components/Navbar";
+import { Inter } from "next/font/google";
+import { ReactNode } from "react";
+import "../styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Jalur Mimpi",
+  description: "Temukan dan wujudkan impianmu melalui peta karir interaktif",
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="id">
+      <body className={inter.className + " bg-gray-50 text-gray-900"}>
+        <Navbar />
+        <main className="max-w-5xl mx-auto p-4">{children}</main>
+      </body>
+    </html>
+  );
+}
